2b54276e5e65f8cdef19c9718b8855734fff0c1d
klieret <klieret@users.noreply.github.com>
2016-02-26 16:00:44 +0900

    cleaner implementation

--- colored_logging.py
+++ colored_logging.py
@@ -2,33 +2,39 @@
 # encoding: utf-8
 
 import logging
+
+# holding different stylings for logs
+lcolor_profiles = dict()
+
+# Default style.
+# feel free to use another module to generate the formatting sequences
+# (or just enter them manually).
+
 import colorama
 from colorama import Fore, Back, Style
+
 colorama.init()
 
+reset_all = Style.RESET_ALL
+lcolor_profiles["default"] = {logging.CRITICAL: Back.BLACK + Fore.RED + Style.BRIGHT,
+                              logging.ERROR:    Back.BLACK + Fore.WHITE + Style.BRIGHT,
+                              logging.WARNING:  Fore.RED + Style.BRIGHT,
+                              logging.INFO:     "",
+                              logging.DEBUG:    Style.DIM}
 
-def level_to_style(level, lvl_to_color=None):
+
+def level_to_style(level, lvl_to_color):
     """
     :param level: Level of the logging message.
     :param lvl_to_color: Supply a dictionary that overrides the default coloring of the form level (int): formatting str
     :return: Terminal control sequence as string.
     """
-    # level to color
-    if not lvl_to_color:
-        # default
-        # either use the keys from $name_to_lvl or just use the level numbers
-        lvl_to_color = {logging.CRITICAL: Back.BLACK + Fore.RED + Style.BRIGHT,
-                        logging.ERROR:    Back.BLACK + Fore.WHITE + Style.BRIGHT,
-                        logging.WARNING:  Fore.RED + Style.BRIGHT,
-                        logging.INFO:     "",
-                        logging.DEBUG:    Style.DIM}
-
     for lvl in sorted(lvl_to_color.keys(), reverse=True):
         if level >= lvl:
             return lvl_to_color[lvl]
 
 
-def return_colored_emit_fct(old_emit_fct):
+def return_colored_emit_fct(old_emit_fct, lvl_to_color=lcolor_profiles["default"]):
     """Returns an emit function/method that automatically adds coloring based ont the level of the logging message.
     :param old_emit_fct: Emit function or method (logging.StreamHandler.emit or logging.StreamHandler().emit)
     :return: emit function/method that automatically adds coloring based on the level of the logging message.
@@ -42,24 +48,28 @@ def return_colored_emit_fct(old_emit_fct):
         #    sh = logging.StreamHanlder()
         #    sh.emit = add_coloring(sh.emit)
         # we get a single argument of type logging.LogReccord.
+        # This function covers both cases.
         for index, arg in enumerate(args):
             if isinstance(arg, logging.LogRecord):
                 break
         else:
             # no break occurred / not found
+            print(args)
             raise(ValueError, "At least one of the arguments has to be of type logging.LogRecord.")
         levelno = args[index].levelno
-        color = level_to_style(levelno)
-        args[index].msg = color + args[index].msg + Style.RESET_ALL
+        color = level_to_style(levelno, lvl_to_color)
+        args[index].msg = color + args[index].msg + reset_all
         return old_emit_fct(*args)
     return colored_emit_fct
 
 
 class ColoredStreamHandler(logging.StreamHandler):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, lvl_to_color=lcolor_profiles["default"], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lvl_to_color = lvl_to_color
 
-ColoredStreamHandler.emit = return_colored_emit_fct(logging.StreamHandler.emit)
+    def emit(self, *args, **kwargs):
+        return_colored_emit_fct(logging.StreamHandler.emit, self.lvl_to_color)(self, *args, **kwargs)
 
 
 if __name__ == "__main__":
