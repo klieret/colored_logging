da1d0d33a67a1061ee354696515a4ba1e87e7b42
klieret <klieret@users.noreply.github.com>
2016-10-23 17:34:47 +0200

    readability improvements & reformatting

--- colored_logging.py
+++ colored_logging.py
@@ -1,17 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import logging
+import colorama
+from colorama import Fore, Back, Style
+from typing import Dict
 
-# holding different stylings for logs
-lcolor_profiles = dict()
+# to provide cross-platform terminal colors, this module uses the colorama
+# module. See https://pypi.python.org/pypi/colorama (e.g. install via
+# sudo pip3 colorama)
+
+# holding different styles for logs
+lcolor_profiles = {}
 
 # Below is the default style.
 # feel free to change the formating or use another module to generate the formatting sequences
 # (you can of course just enter them manually).
 
-import colorama
-from colorama import Fore, Back, Style
+
 
 colorama.init()
 
@@ -30,45 +36,58 @@ lcolor_profiles["dim"] = {0: Style.DIM}
 lcolor_profiles["black"] = {}
 
 
-def level_to_style(level, lvl_to_color):
+def level_to_style(level: int, lcolor_profile: Dict[int, str]) -> str:
     """
     :param level: Level of the logging message.
-    :param lvl_to_color: A dict defining coloring of each logging message. Format: {level (int): formatting str}.
+    :param lcolor_profile: A dict defining coloring of each logging message.
+                           Format: {level (int): formatting str}.
     :return: Terminal control sequence as string.
     """
-    for lvl in sorted(lvl_to_color.keys(), reverse=True):
+    for lvl in sorted(lcolor_profile.keys(), reverse=True):
         if level >= lvl:
-            return lvl_to_color[lvl]
+            return lcolor_profile[lvl]
     else:
-        # no return occurred
         return ""
 
 
-def return_colored_emit_fct(old_emit_fct, lvl_to_color=lcolor_profiles["default"]):
-    """Returns an emit function/method that automatically adds coloring based ont the level of the logging message.
-    :param old_emit_fct: Emit function or method (logging.StreamHandler.emit or logging.StreamHandler().emit)
-    :param lvl_to_color: A dict defining coloring of each logging message. Format: {level (int): formatting str}.
-    :return: emit function/method that automatically adds coloring based on the level of the logging message.
-    """
+def return_colored_emit_fct(old_emit_fct,
+                            lvl_to_color=lcolor_profiles["default"]):
+    """Returns an emit function/method that automatically adds coloring based
+    on the logging level.
+    :param old_emit_fct: Original Emit function or method:
+                         logging.StreamHandler.emit or l
+                         ogging.StreamHandler().emit)
+    :param lvl_to_color: A dict defining coloring of each logging message.
+                         Format: {level (int): formatting str}.
+    :return: emit function/method that adds coloring based on the
+             logging level"""
     def colored_emit_fct(*args):
-        # Depending on how we use the add_coloring function,
-        # the arguments will be different: If we we do
-        #    logging.StreamHandler.emit = add_coloring(logging.StreamHandler.emit)
-        # the argument types will be (logging.StreamHandler, logging.LogRecord),
-        # if we do
+        # Depending on how you use this moddle/return_colored_emit_fct,
+        # the arguments that will later be given to colored_emit_fct will be
+        # different:
+        # * Case 1:
+        #  logging.StreamHandler.emit = add_coloring(logging.StreamHandler.emit)
+        # In this case, colored_emit_fct will be called with the arguments
+        # (logging.StreamHandler, logging.LogRecord).
+        #
+        # * Case 2:
         #    sh = logging.StreamHanlder()
         #    sh.emit = add_coloring(sh.emit)
-        # we get a single argument of type logging.LogReccord.
-        # This function covers both cases.
+        # In this case, colored_emit_fct will be called with the argument
+        # logging.LogRecord.
+        #
+        # colored_emit_fct can handle both cases.
         for index, arg in enumerate(args):
             if isinstance(arg, logging.LogRecord):
                 break
         else:
-            # no break occurred / not found ==> abort
-            print("Got the following args:")
-            for arg in args:
-                print("Arg", arg, "of type", type(arg))
-            raise(ValueError, "At least one of them snould have been of type logging.LogRecord.")
+            # no 'break' occurred / LogRecord not found ==> abort
+            abort_msg = ["Got the following args:"]
+            abort_msg.extend(["Arg {} of type {}".format(arg, type(arg))
+                              for arg in args])
+            abort_msg.append("At least one of them should have been of type "
+                             "logging.LogRecord.")
+            raise(ValueError, '\n'.join(abort_msg))
         levelno = args[index].levelno
         color = level_to_style(levelno, lvl_to_color)
         args[index].msg = color + args[index].msg + reset_all
@@ -102,25 +121,34 @@ def demo_profile(color_profile, name=""):
     sh = ColoredStreamHandler(lvl_to_color=color_profile)
     sh.setFormatter(formatter)
 
-    # unique name (else logging.getLogger will return the same logger every time)
+    # unique name (else logging.getLogger will return the same logger every
+    # time)
     lname = str(id(color_profile))
 
     logger = logging.getLogger(lname)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(sh)
 
-    # make sure to test both the standard levels and if nescessary user defined levels.
-    standard_levels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]
-    test_levels = set(list((color_profile.keys())) + standard_levels)  # (set: make sure to not have duplicates)
+    # make sure to test both the standard levels and if nescessary user defined
+    # levels.
+    standard_levels = [logging.DEBUG, logging.INFO, logging.WARNING,
+                       logging.ERROR, logging.CRITICAL]
+    # make sure to not have duplicates
+    all_levels = set(list((color_profile.keys())) + standard_levels)
 
-    for lvl in sorted(test_levels, reverse=True):
-        record = logging.LogRecord(name=lname, level=lvl, pathname=__file__, lineno=86,
-                                   msg="Logging message of level {} ({})".format(lvl, logging.getLevelName(lvl)),
-                                   args={}, exc_info=None)
+    for lvl in sorted(all_levels, reverse=True):
+        msg = "Logging message of level {} ({})".format(lvl, logging.getLevelName(lvl))
+        record = logging.LogRecord(name=lname,
+                                   level=lvl,
+                                   pathname=__file__,
+                                   lineno=86,
+                                   msg=msg,
+                                   args={},
+                                   exc_info=None)
         logger.handle(record)
 
 
 if __name__ == "__main__":
-    print("*** TESTING LOG COLOR PROFILES ***")
+    print("*** TESTING COLOR PROFILES ***")
     for profile_name in lcolor_profiles:
         demo_profile(lcolor_profiles[profile_name], profile_name)
