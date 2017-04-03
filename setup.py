4bfb00bb7acf1fcdb8bcf0f5ddfbc3a0fcd74f96
klieret <klieret@users.noreply.github.com>
2017-04-03 10:02:21 +0200

    more information for setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,17 @@ setup(name='colorlog',
       description='Color for the python3 logging module',
       author='klieret',
       author_email='klieret@users.noreply.github.com',
+      maintainer='klieret',
+      maintainer_email'klieret@users.noreply.github.com',
       url='https://github.com/klieret/python-colorlog',
       packages=['colorlog'],
-      install_requires=['typing'],)
-
-
-
+      install_requires=['typing'],
+      license='GNU Lesser General Public License v3.0',
+      classifiers = [
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        ]
+      )
 
